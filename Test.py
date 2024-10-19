891607ad5cfdc8f7c3e348292dfd05d945b2ab79
S2424140 <marwanbadawi@outlook.com>
2024-10-19 09:02:50 +0100

    test 2 commit

--- Test.py
+++ Test.py
@@ -1,4 +1,14 @@
 print("hello")
 
-while (True):
-	print("we are going to win")
+motivation = True
+
+energy = 10
+
+while motivation:
+    print("we are going to win")
+    energy -= 1
+    if energy == 0:
+        motivation = False
+
+print("I sleep")
+
