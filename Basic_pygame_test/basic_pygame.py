fd3b4eb3d830d463ff0a7fba77ba6b76ca370a49
Davvv24 <rizzofamily3@gmail.com>
2024-10-19 15:42:12 +0100

    I added changes?

--- Basic_pygame_test/basic_pygame.py
+++ Basic_pygame_test/basic_pygame.py
@@ -1,7 +1,7 @@
 import pygame
-from Sprite_Services.Basic_player import Player
-from Sprite_Services.Market_Stall import MarketStall
-from Sprite_Services.Buttons import Button
+from Basic_player import Player
+from Market_Stall import MarketStall
+from Buttons import Button
 
 
 from Basic_player import Player
