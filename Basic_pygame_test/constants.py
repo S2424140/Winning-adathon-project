a22bff694acd6f0e4d5d956f98ba664551957f68
S2424140 <marwanbadawi@outlook.com>
2024-10-19 12:54:41 +0100

    added a shadow to the market stall

--- Basic_pygame_test/constants.py
+++ Basic_pygame_test/constants.py
@@ -4,3 +4,4 @@ size = width, height = 800, 400
 
 D_Idle_path = "Assets/Player-Animations/D_Idle.png"
 Market_Stall_path = "Assets/Buildings/Objects/6 Tent/1.png"
+Market_Stall_Shadow_path = "Assets/Buildings/Objects/1 Shadow/6.png"
